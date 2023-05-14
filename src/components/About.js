01cd134bc58901450758cefdbdc4962e55c5231b
Corrine <corrine.sing@gmail.com>
2023-05-14 22:15:17 +0100

    worked on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./About.css";
 
 const About = () => {
     return (
         <>
-            <h1>About</h1>
-            <p>Esse occaecat ea ut sit laborum Lorem in commodo proident. Aliquip et tempor laboris velit deserunt labore ut duis non fugiat. Occaecat duis ex ea et ipsum laborum aute reprehenderit dolor nostrud deserunt enim id. Aliquip et ea qui ex amet anim cupidatat ex culpa commodo pariatur. Sit est tempor duis nostrud sint velit. Aliqua reprehenderit elit ad exercitation anim nulla fugiat cillum ut in tempor pariatur.</p>
-            
+            <div className="about-container">
+                <h1 className="about-title">About</h1>
+                
+                <p>
+                    Esse occaecat ea ut sit laborum Lorem in commodo proident. Aliquip et
+                    tempor laboris velit deserunt labore ut duis non fugiat. Occaecat duis
+                    ex ea et ipsum laborum aute reprehenderit dolor nostrud deserunt enim
+                    id. Aliquip et ea qui ex amet anim cupidatat ex culpa commodo
+                    pariatur. Sit est tempor duis nostrud sint velit. Aliqua reprehenderit
+                    elit ad exercitation anim nulla fugiat cillum ut in tempor pariatur.
+                </p>
+            </div>
         </>
-    )
+    );
 };
 
 export default About;
-
