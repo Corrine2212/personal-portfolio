5a52568625a1b1aef21c9fb7a6bb9f47b83e57f6
Corrine <corrine.sing@gmail.com>
2023-05-15 00:44:45 +0100

    Updates

---
+++ precache-manifest.381f535ee7fe3c1b034272c34ac66fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3614fd636658942a0a95",
+    "url": "/personal-portfolio/static/css/main.bc396d61.chunk.css"
+  },
+  {
+    "revision": "3614fd636658942a0a95",
+    "url": "/personal-portfolio/static/js/main.3614fd63.chunk.js"
+  },
+  {
+    "revision": "046e05133628af1d8cc8",
+    "url": "/personal-portfolio/static/js/1.046e0513.chunk.js"
+  },
+  {
+    "revision": "6d2f2ccdb32ebb52fd46",
+    "url": "/personal-portfolio/static/js/2.6d2f2ccd.chunk.js"
+  },
+  {
+    "revision": "08146ffd00a4145841bf",
+    "url": "/personal-portfolio/static/js/runtime~main.08146ffd.js"
+  },
+  {
+    "revision": "94568da51f7d4e298d2736e2dfabff15",
+    "url": "/personal-portfolio/index.html"
+  }
+];
